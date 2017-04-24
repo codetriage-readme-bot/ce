c1c618b2e948d3ddc9dc36eff06195f18ac1e002
atombugtrackdev <atombt@protonmail.com>
2017-04-24 09:39:43 +0200

    sql, in 2017 damn!

---
+++ sql.sql
@@ -0,0 +1,405 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Creato il: Apr 23, 2017 alle 21:36
+-- Versione del server: 10.1.13-MariaDB
+-- Versione PHP: 5.6.21
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `test`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `aauth_groups`
+--
+
+CREATE TABLE `aauth_groups` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `name` varchar(100) DEFAULT NULL,
+  `definition` text
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dump dei dati per la tabella `aauth_groups`
+--
+
+INSERT INTO `aauth_groups` (`id`, `name`, `definition`) VALUES
+(1, 'Admin', 'Super Admin Group'),
+(2, 'Enterprise', 'Enterprise User Group'),
+(3, 'Public', 'Public Access Group'),
+(4, 'Default', 'Default Access Group'),
+
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `aauth_group_to_group`
+--
+
+CREATE TABLE `aauth_group_to_group` (
+  `group_id` int(11) UNSIGNED NOT NULL,
+  `subgroup_id` int(11) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `aauth_login_attempts`
+--
+
+CREATE TABLE `aauth_login_attempts` (
+  `id` int(11) NOT NULL,
+  `ip_address` varchar(39) DEFAULT '0',
+  `timestamp` datetime DEFAULT NULL,
+  `login_attempts` tinyint(2) DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `aauth_perms`
+--
+
+CREATE TABLE `aauth_perms` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `name` varchar(100) DEFAULT NULL,
+  `definition` text
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dump dei dati per la tabella `aauth_perms`
+--
+
+INSERT INTO `aauth_perms` (`id`, `name`, `definition`) VALUES
+(1, 'enterprise_user', ''),
+(2, 'opensource_user', ''),
+(3, 'paid', '');
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `aauth_perm_to_group`
+--
+
+CREATE TABLE `aauth_perm_to_group` (
+  `perm_id` int(11) UNSIGNED NOT NULL,
+  `group_id` int(11) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dump dei dati per la tabella `aauth_perm_to_group`
+--
+
+INSERT INTO `aauth_perm_to_group` (`perm_id`, `group_id`) VALUES
+(1, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `aauth_perm_to_user`
+--
+
+CREATE TABLE `aauth_perm_to_user` (
+  `perm_id` int(11) UNSIGNED NOT NULL,
+  `user_id` int(11) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `aauth_pms`
+--
+
+CREATE TABLE `aauth_pms` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `sender_id` int(11) UNSIGNED NOT NULL,
+  `receiver_id` int(11) UNSIGNED NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `message` text,
+  `date_sent` datetime DEFAULT NULL,
+  `date_read` datetime DEFAULT NULL,
+  `pm_deleted_sender` int(1) DEFAULT NULL,
+  `pm_deleted_receiver` int(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `aauth_users`
+--
+
+CREATE TABLE `aauth_users` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `pass` varchar(64) NOT NULL,
+  `username` varchar(100) DEFAULT NULL,
+  `banned` tinyint(1) DEFAULT '0',
+  `last_login` datetime DEFAULT NULL,
+  `last_activity` datetime DEFAULT NULL,
+  `date_created` datetime DEFAULT NULL,
+  `forgot_exp` text,
+  `remember_time` datetime DEFAULT NULL,
+  `remember_exp` text,
+  `verification_code` text,
+  `totp_secret` varchar(16) DEFAULT NULL,
+  `ip_address` text
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dump dei dati per la tabella `aauth_users`
+--
+
+INSERT INTO `aauth_users` (`id`, `email`, `pass`, `username`, `banned`, `last_login`, `last_activity`, `date_created`, `forgot_exp`, `remember_time`, `remember_exp`, `verification_code`, `totp_secret`, `ip_address`) VALUES
+(1, 'admin@admin.com', '3f154db4e6a57bbfabbffcf30fc7016ae67027a4ae36d9787750b623734bfc41', '', 0, '2017-04-21 15:45:42', '2017-04-21 15:45:42', '2017-04-12 14:22:26', NULL, NULL, NULL, NULL, NULL, '::1');
+/* admin@admin.com per loggarsi da dashboard e come password atompass */
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `aauth_user_to_group`
+--
+
+CREATE TABLE `aauth_user_to_group` (
+  `user_id` int(11) UNSIGNED NOT NULL,
+  `group_id` int(11) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dump dei dati per la tabella `aauth_user_to_group`
+--
+
+INSERT INTO `aauth_user_to_group` (`user_id`, `group_id`) VALUES
+(1, 1),
+(1, 3),
+(2, 4);
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `aauth_user_variables`
+--
+
+CREATE TABLE `aauth_user_variables` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `user_id` int(11) UNSIGNED NOT NULL,
+  `data_key` varchar(100) NOT NULL,
+  `value` text
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `issue`
+--
+
+CREATE TABLE `issue` (
+  `id` int(11) NOT NULL,
+  `issue_id` varchar(32) DEFAULT NULL,
+  `title` varchar(128) NOT NULL DEFAULT 'Default title',
+  `slug` varchar(128) NOT NULL,
+  `project` varchar(128) DEFAULT NULL,
+  `milestone` varchar(128) DEFAULT NULL,
+  `type` varchar(128) NOT NULL DEFAULT 'Bug',
+  `text` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dump dei dati per la tabella `issue`
+--
+
+INSERT INTO `issue` (`id`, `issue_id`, `title`, `slug`, `project`, `milestone`, `type`, `text`) VALUES
+(1, NULL, 'New issue', '', 'project 1', 'milestone', 'Bug', 'This is the first bug, Welcome to AtomBT'),
+
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `projects`
+--
+
+CREATE TABLE `projects` (
+  `id` int(11) NOT NULL,
+  `title` varchar(128) NOT NULL DEFAULT 'Default title',
+  `slug` varchar(128) NOT NULL,
+  `text` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dump dei dati per la tabella `projects`
+--
+
+INSERT INTO `projects` (`id`, `title`, `slug`, `text`) VALUES
+(1, 'Project Title', 'project', 'il progetto è bello finchè dura pcoo'),
+(2, 'Project Title 2', 'project', '2 il progetto è bello finchè dura pcoo');
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `type`
+--
+
+CREATE TABLE `type` (
+  `id` int(11) NOT NULL,
+  `name` varchar(64) NOT NULL,
+  `default` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dump dei dati per la tabella `type`
+--
+
+INSERT INTO `type` (`id`, `name`, `default`) VALUES
+(1, 'Bug', 0),
+(2, 'Feature', 0);
+(3, 'Task',0);
+
+--
+-- Indici per le tabelle scaricate
+--
+
+--
+-- Indici per le tabelle `aauth_groups`
+--
+ALTER TABLE `aauth_groups`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indici per le tabelle `aauth_group_to_group`
+--
+ALTER TABLE `aauth_group_to_group`
+  ADD PRIMARY KEY (`group_id`,`subgroup_id`);
+
+--
+-- Indici per le tabelle `aauth_login_attempts`
+--
+ALTER TABLE `aauth_login_attempts`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indici per le tabelle `aauth_perms`
+--
+ALTER TABLE `aauth_perms`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indici per le tabelle `aauth_perm_to_group`
+--
+ALTER TABLE `aauth_perm_to_group`
+  ADD PRIMARY KEY (`perm_id`,`group_id`);
+
+--
+-- Indici per le tabelle `aauth_perm_to_user`
+--
+ALTER TABLE `aauth_perm_to_user`
+  ADD PRIMARY KEY (`perm_id`,`user_id`);
+
+--
+-- Indici per le tabelle `aauth_pms`
+--
+ALTER TABLE `aauth_pms`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `full_index` (`id`,`sender_id`,`receiver_id`,`date_read`);
+
+--
+-- Indici per le tabelle `aauth_users`
+--
+ALTER TABLE `aauth_users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indici per le tabelle `aauth_user_to_group`
+--
+ALTER TABLE `aauth_user_to_group`
+  ADD PRIMARY KEY (`user_id`,`group_id`);
+
+--
+-- Indici per le tabelle `aauth_user_variables`
+--
+ALTER TABLE `aauth_user_variables`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id_index` (`user_id`);
+
+--
+-- Indici per le tabelle `issue`
+--
+ALTER TABLE `issue`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `slug` (`slug`);
+
+--
+-- Indici per le tabelle `projects`
+--
+ALTER TABLE `projects`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `slug` (`slug`);
+
+--
+-- Indici per le tabelle `type`
+--
+ALTER TABLE `type`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT per le tabelle scaricate
+--
+
+--
+-- AUTO_INCREMENT per la tabella `aauth_groups`
+--
+ALTER TABLE `aauth_groups`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+--
+-- AUTO_INCREMENT per la tabella `aauth_login_attempts`
+--
+ALTER TABLE `aauth_login_attempts`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT per la tabella `aauth_perms`
+--
+ALTER TABLE `aauth_perms`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT per la tabella `aauth_pms`
+--
+ALTER TABLE `aauth_pms`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT per la tabella `aauth_users`
+--
+ALTER TABLE `aauth_users`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT per la tabella `aauth_user_variables`
+--
+ALTER TABLE `aauth_user_variables`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT per la tabella `issue`
+--
+ALTER TABLE `issue`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+--
+-- AUTO_INCREMENT per la tabella `projects`
+--
+ALTER TABLE `projects`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT per la tabella `type`
+--
+ALTER TABLE `type`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
